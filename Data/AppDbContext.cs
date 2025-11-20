4130baaa99ff8764587b736def4297dfe84a4090
saujanyamahajan <saujanya.9919@gmail.com>
2025-11-20 10:00:00 +0530

    inheriting Db context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,5 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using EmployeeManagement.API.Models;
 namespace EmployeeManagement.API.Data
 {
-    
+    public class AppDbContext:DbContext
+    {
+        
+    }
 }
