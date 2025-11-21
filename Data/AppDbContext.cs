9a6604f120efb6d5e8841cad9c683b78ddf5abd6
saujanyamahajan <saujanya.9919@gmail.com>
2025-11-21 10:00:00 +0530

    adding Db context options

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -4,6 +4,7 @@ namespace EmployeeManagement.API.Data
 {
     public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
         
     }
 }
