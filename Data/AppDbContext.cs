f7d7e52ff1d712b6e272dfa03e9d5f2992ba4cdb
saujanyamahajan <saujanya.9919@gmail.com>
2025-11-22 10:00:00 +0530

    connect model and data layer

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,6 +5,8 @@ namespace EmployeeManagement.API.Data
     public class AppDbContext:DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
-        
+
+        public DbSet<Employee> Employees {get;set;}
+
     }
 }
