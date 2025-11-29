b3b590e93fd797e144aa07bf12ed6c526bb3a02d
saujanyamahajan <saujanya.9919@gmail.com>
2025-11-29 10:00:00 +0530

    injecting dependency in employee controller

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -6,5 +6,17 @@ using EmployeeManagement.API.Models;
 
  namespace EmployeeManagement.API.Controllers
 {
-    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        //dependency injection
+        public  EmployeeController(AppDbContext context)
+        {
+            _context=context;
+        }
+
+    }
 }
